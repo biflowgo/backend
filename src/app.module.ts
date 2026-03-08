a113962604d622fef63100abef49a6510eac80ec
starmaster1990 <ronny.tigrero.22@gmail.com>
2026-03-08 15:07:42 -0500

    :sparkles: feature:Implementation for Postgres in the project
     Instalation of libraries and configguration in main module #2

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,26 @@ import { CategoryModule } from './category/category.module';
 import { ProductModule } from './product/product.module';
 import { BusinessModule } from './business/business.module';
 import { IdModule } from './core/id/id.module';
-
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [CategoryModule, ProductModule, BusinessModule, IdModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true, // Automatically load entities from the project
+      synchronize: true,// Note: Set synchronize to false in production
+    }),
+    CategoryModule,
+    ProductModule, 
+    BusinessModule, 
+    IdModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
